26f359d84f7b58b742b752e3cdf6a6f419382d70
Saup21 <sauparna.gupta@gmail.com>
2021-08-03 20:32:51 +0530

    Winner checker done

--- src/components/Board.js
+++ src/components/Board.js
@@ -30,7 +30,8 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        const winner = calculateWinner(this.state.squares);
+        const status = winner ? `Winner ${winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
             <div>
@@ -53,6 +54,26 @@ class Board extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 export default Board
